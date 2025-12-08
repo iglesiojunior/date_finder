d5d7da824dd8bab3b285b20470fcdb257ab96e54
Iglesio Júnior <catce.2024111tads0014@aluno.ifpi.edu.br>
2025-12-08 09:46:25 -0300

    :sparkles: feat:new configuration to render enviroment

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:app_links/app_links.dart'; // Importe o pacote
 import 'views/login_page.dart';
+import 'views/reset_password_page.dart'; // Importe a tela de reset
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _navigatorKey = GlobalKey<NavigatorState>(); // Chave para navegação
+  late AppLinks _appLinks;
+
+  @override
+  void initState() {
+    super.initState();
+    _initDeepLinks();
+  }
+
+  Future<void> _initDeepLinks() async {
+    _appLinks = AppLinks();
+
+    // Verifica link inicial (se o app estava fechado e foi aberto pelo link)
+    final Uri? initialUri = await _appLinks.getInitialUri();
+    if (initialUri != null) {
+      _handleDeepLink(initialUri);
+    }
+
+    // Escuta links enquanto o app está aberto ou em background
+    _appLinks.uriLinkStream.listen((Uri? uri) {
+      if (uri != null) {
+        _handleDeepLink(uri);
+      }
+    }, onError: (err) {
+      debugPrint('Erro no Deep Link: $err');
+    });
+  }
+
+  void _handleDeepLink(Uri uri) {
+    // O formato esperado é: datefinder://reset-password?token=XYZ
+    if (uri.host == 'reset-password') {
+      final String? token = uri.queryParameters['token'];
+      
+      if (token != null) {
+        // Navega para a tela de Reset enviando o token
+        _navigatorKey.currentState?.push(
+          MaterialPageRoute(
+            builder: (context) => ResetPasswordPage(token: token),
+          ),
+        );
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigatorKey, // Importante vincular a chave aqui
       title: 'Date Spot Finder',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -18,5 +70,5 @@ class MyApp extends StatelessWidget{
       ),
       home: const LoginPage(),
     );
-    }
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import geolocator_apple
 import package_info_plus
 import shared_preferences_foundation
@@ -12,6 +13,7 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
